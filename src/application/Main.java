e5a3922bdf577c7315f1e97fcf1297f07254aa96
helidesenvolve <helidesenvolve@gmail.com>
2020-06-10 12:25:07 -0300

    MainView Controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,6 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 
